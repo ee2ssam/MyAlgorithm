eb3e6635097818bdf05ac9eaad781e28717ada8f
ee2ssam <97661118+ee2ssam@users.noreply.github.com>
2025-08-07 13:59:18 +0900

    1. 배열 예제

--- MyAlgorithm/ArrayTest/ArrayTest.cpp
+++ MyAlgorithm/ArrayTest/ArrayTest.cpp
@@ -2,30 +2,190 @@
 //
 
 #include <iostream>
+#include <array>
+#include <vector>
 
 using namespace std;
 
-//학생 5명의 점수들의 평균을 구하라 - C 언어 배열 구현
+
+//가변형 배열(vector) 2차원 배열 사용 예제
 int main()
 {
-	int scores[5] = { 50, 60, 70, 80, 90 };
+	//2행 3열로 생성하고 0으로 초기화
+	vector<vector<int>> mat1(2, vector<int>(3, 0));
 
-	//배열 길이 얻어오기
-	int length = size(scores);
+	//2행 3열로 생성하고 초기값 설정
+	vector<vector<int>> mat2{
+		{1, 2, 3},
+		{4, 5, 6}
+	};
 
-	//합계 구하기
-	int sum = 0;
-	for (int i = 0; i < length; i++)
+	//출력
+	for (int r = 0; r < mat2.size(); r++)
 	{
-		sum += scores[i];
+		for (int c = 0; c < mat2[r].size(); c++)
+		{
+			cout << mat2[r][c] << " ";
+		}
+		cout << endl;
 	}
-
-	//평균 구하기
-	float avg = (float)sum / length;
-
-	//콘솔 출력
-	cout << "Mean Score: " << avg << endl;
 }
+
+////가변형 배열(vector) 사용 예제
+//int main()
+//{
+//	vector<int> v1;	
+//	v1.push_back(10);
+//	v1.push_back(20);
+//	v1.push_back(30);
+//
+//	vector<int> v2(10);		//0으로 초기화된 10개의 방 생성
+//	vector<int> v3(10, 1);	//1로 초기화된 10개의 방 생성
+//	vector<int> v4{ 10, 20, 30, 40, 50 };
+//
+//	vector<int> v5(v4);
+//	vector<int> v6(v4.begin(), v4.begin() + 3);
+//	for (int i = 0; i < v6.size(); i++)
+//	{
+//		cout << v6[i] << endl;
+//	}
+//}
+
+////할당된 동적 메모리가 자동으로 해제되는 탬플릿<T> 배열 클래스
+//// [], 크기
+//template<typename T>
+//class DynamicArray
+//{
+//private:
+//	unsigned int sz;			//배열의 크기
+//	T* arr;						//배열<T> 포인터
+//	
+//public:
+//	//생성자 - 동적 메모리 할당, 배열의 크기 결정
+//	DynamicArray(int n) : sz(n)
+//	{
+//		arr = new T[sz] {};
+//	}
+//
+//	//소멸자 - 할당된 동적 메모리를 해제
+//	~DynamicArray()
+//	{
+//		delete[] arr;
+//		cout << "동적 메모리 해제" << endl;
+//	}
+//
+//	//배열 크기
+//	unsigned int size()
+//	{
+//		return sz;
+//	}
+//
+//	//[]
+//	T& operator[] (const int i) { return arr[i]; }
+//};
+//
+////DynamicArray 예제
+//
+//int main()
+//{
+//	DynamicArray<int> da(5);
+//	da[0] = 10;
+//	da[1] = 20;
+//	da[2] = 30;
+//
+//	for (int i = 0; i < da.size(); i++)
+//	{
+//		cout << da[i] << endl;
+//	}
+//}
+
+////동적 메모리 할당-해제
+//int main()
+//{
+//	//동적 메모리 할당
+//	int* ptr = new int[3] {};
+//	ptr[0] = 10;
+//	ptr[1] = 20;
+//
+//	//사용
+//	for (int i = 0; i < 3; i++)
+//	{
+//		cout << ptr[i] << endl;
+//	}
+//
+//	//할당된 동적 메모리 해제
+//	delete[] ptr;
+//	ptr = nullptr;
+//}
+
+////학생 5명의 점수들의 평균을 구하라 - C++ 언어 array 구현
+//int main()
+//{
+//	array<int, 5> scores = { 50, 60, 70, 80, 90 };
+//
+//	//합계 구하기
+//	int sum = 0;
+//	for (int i = 0; i < scores.size(); i++)
+//	{
+//		sum += scores[i];
+//	}
+//
+//	//평균 구하기
+//	float avg = (float)sum / scores.size();
+//
+//	//출력
+//	cout << "Mean Score: " << avg << endl;
+//}
+
+
+////2차원 배열 - C 언어 배열
+//int main()
+//{
+//	int mat[2][3] = {
+//		{1, 2, 3},
+//		{4, 5, 6}
+//	};
+//
+//	//2차원 배열의 출력하고 합 구하기
+//	int sum = 0;
+//	for (int r = 0; r < 2; r++)
+//	{
+//		for (int c = 0; c < 3; c++)
+//		{
+//			cout << mat[r][c] << " ";
+//
+//			sum += mat[r][c];
+//		}
+//		cout << endl;
+//	}
+//
+//	cout << "Total: " << sum << endl;
+//}
+//
+
+
+
+////학생 5명의 점수들의 평균을 구하라 - C 언어 배열 구현
+//int main()
+//{
+//	int scores[5] = { 50, 60, 70, 80, 90 };
+//
+//	//배열 길이 얻어오기
+//	int length = size(scores);
+//
+//	//합계 구하기
+//	int sum = 0;
+//	for (int i = 0; i < length; i++)
+//	{
+//		sum += scores[i];
+//	}
+//
+//	//평균 구하기
+//	float avg = (float)sum / length;
+//
+//	//콘솔 출력
+//	cout << "Mean Score: " << avg << endl;
+//}
 
 
 
---
+++ MyAlgorithm/ListTest/ListTest.cpp
@@ -0,0 +1,102 @@
+﻿// ListTest.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+//int data를 가진 node list 구현
+struct Node
+{
+	int data;
+	Node* next;
+};
+
+class LinkedList
+{
+private:
+	Node* head;
+
+public:
+	//생성자
+	LinkedList() : head(NULL) {};
+
+	//소멸자
+	~LinkedList()
+	{
+
+	}
+
+	//리스트 맨 앞에 데이터(value) 삽입하기
+	void push_pront(int value)
+	{
+		//삽입할 노드 생성 (data:value, next:NULL 초기화)
+		Node* new_node = new Node{ value , NULL };
+
+		//데이터 있으면
+		if (head != NULL)
+		{
+			new_node->next = head;
+		}
+
+		head = new_node;
+	}
+
+	//리스트 맨 앞에 있는 데이터 삭제
+	void pop_front()
+	{
+		//데이터 없으면
+		if (head == NULL)
+		{
+			return;
+		}
+
+		Node* first = head;
+		head = first->next;
+		delete first;
+	}
+
+	//리스트의 모든 노드 데이터 출력하기
+	void print_all()
+	{
+		Node* curr = head;
+
+		//반복문
+		while (curr != NULL)
+		{
+			cout << curr->data << ", ";
+			//노드 자신의 next로 다음 노드의 주소를 가져온다
+			curr = curr->next;
+		}
+		cout << endl;
+	}
+
+	//데이터 유무 체크
+	bool empty()
+	{
+		return head == NULL;
+	}
+};
+
+
+
+int main()
+{
+
+}
+
+//int main()
+//{
+//    std::cout << "Hello List!\n";
+//}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
